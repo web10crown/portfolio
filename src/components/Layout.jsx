9633375cd23955bc54fba127c8869e5fbcb8f593
web10crown <99588906+web10crown@users.noreply.github.com>
2023-03-01 19:37:22 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,8 +1,19 @@
+import Head from "next/head";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 const Layout = ({ children }) => {
   return (
     <>
+      <Head>
+        <title>Neeraj Rana | India</title>
+        <meta name="description" content="Portfolio website for Projects display" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          rel="icon"
+          href="/avatar/profile.jpeg"
+          
+        />
+      </Head>
       <Navbar />
       {children}
       <Footer />

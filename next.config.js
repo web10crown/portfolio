23ae3067c8daf682fab9b899c4f3122c3a76c799
web10crown <99588906+web10crown@users.noreply.github.com>
2023-03-02 00:00:25 +0530

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,22 @@
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com"],
+  },
+  async headers() {
+    const headers = [];
+
+    headers.push({
+      headers: [
+        {
+          key: "X-Robots-Tag",
+          value: "index",
+        },
+      ],
+      source: "/:path*",
+    });
+
+    return headers;
+  },
+};
+
+module.exports = nextConfig
